d846f81797c90e7af7d4a2e9ffbc1c7ad2492b87
Paolo Giordani <paolo.giordani@bi.no>
2021-12-15 17:15:50 +0100

    Small change to PSodelrindPrintTable

--- src/PSoderlindPrintTable.jl
+++ src/PSoderlindPrintTable.jl
@@ -119,7 +119,7 @@ end
 #    printmat(stdout::IO,x...;colNames,rowNames,width,prec,NoPrinting,StringFmt,cell00)
 
 printmat(x...;colNames=[],rowNames=[],width=10,prec=3,NoPrinting=false,StringFmt="",cell00="") =
-    printmat(stdout::IO,x...;colNames=colNames,rowNames=rowNames,width=width,prec=prec,NoPrinting=NoPrinting,StringFmt=StringFmt,cell00=cell00)
+    printmat(stdout::IO,x...;colNames=[],rowNames=[],width=10,prec=3,NoPrinting=false,StringFmt="",cell00="")
 
 
 #------------------------------------------------------------------------------
@@ -154,8 +154,8 @@ end
                       #when fh is not supplied: printing to screen
 # Paolo Giordani: Paul's original syntax requires Julia 1.6
 #printlnPs(z...;width=10,prec=3) = printlnPs(stdout::IO,z...;width,prec)
-printlnPs(z...;width=10,prec=3) = printlnPs(stdout::IO,z...;width=width,prec=prec)
-
+printlnPs(z...;width=10,prec=3) = printlnPs(stdout::IO,z...;width=10,prec=3)
+        
 #------------------------------------------------------------------------------
 
 
