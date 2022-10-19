52f994837f13845123aad826b296232ce81f5382
Paolo Giordani <paolo.giordani@bi.no>
2022-10-19 14:30:41 +0200

    Update fit.jl

--- src/fit.jl
+++ src/fit.jl
@@ -416,7 +416,7 @@ function preparedataSMART(data,param)
         end
     end
 
-    data_standardized = SMARTdata(data.y,(data.x .- meanx)./stdx,param,data.dates,weights=data.weights)
+    data_standardized = SMARTdata(data.y,(data.x .- meanx)./stdx,param,data.dates)
 
     return data_standardized,meanx,stdx
 end
